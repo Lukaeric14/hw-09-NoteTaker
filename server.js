196d7c634ba36320a4011fdf6f7907b3957565c9
Luka <lukaericnu@gmail.com>
2022-09-03 18:00:29 -0500

    edits

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 // GET Route for homepage
