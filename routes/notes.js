0ab6b7062c2e42f797a957386d9d18b2ac5662d8
Luka <lukaericnu@gmail.com>
2022-09-06 09:04:21 -0500

    edit

---
+++ routes/notes.js
@@ -0,0 +1,51 @@
+const notes = require('express').Router();
+const fs = require('fs');
+const path = require('path')
+const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+notes.get('/', (req, res) => {
+    res.json(db)
+})
+
+
+// POST Route
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+        const note = {
+            title, text, id: uuidv4(),
+        };
+
+        db.push(note);
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4))
+        res.json(response);
+});
+
+notes.delete('/:id', (req, res) => {
+
+    const Id = req.params.id;
+
+    db.splice(Id - 1,1);
+
+    db.forEach((obj, i) => {
+        
+      obj.id = i + 1;
+    });
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(newData, null, 4), function () {
+      res.json(`${id}`);
+    });
+
+    const response = {
+      status: 'success',
+      body: newData,
+    };
+
+    console.log(response);
+  
+});
+  
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const db = require('./db/db.json');
-const { v4: uuidv4 } = require('uuid');
+const api = require('./routes/routes.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,10 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 app.use(express.static('public'));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -22,35 +20,5 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-
-
-    const { title, text } = req.body;
-
-  if (req.body) {
-    const note = {
-        title,
-        text,
-        id: uuidv4(),
-    };
-  console.log(req.body);
-
-  db.push(note);
-
-  fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4))
-
-  const response = {
-    status: 'success',
-    body: newNote,
-  };
-
-
-  res.json(response)
-} else {
-  res.json('Error in posting feedback');
-}
-
-});
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+ 
